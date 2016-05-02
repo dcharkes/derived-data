4fe6178f1fa06458b9553b70f50cf6626de77a5c
Daco Harkes <dc.harkes@gmail.com>
2016-05-02 12:54:06 +0200

    WebLab grades: REScala implementation (fix + explanation)

--- Weblab-Grades/REScala/src/WeblabGradesREScala.scala
+++ Weblab-Grades/REScala/src/WeblabGradesREScala.scala
@@ -4,7 +4,7 @@ import makro.SignalMacro.{ SignalM => Signal }
 
 object WeblabGradesREScala extends App {
 
-  val mathAlice = new Submission() // throws Exception
+  val mathAlice = new Submission()
   val examAlice = new Submission()
   val labAlice = new Submission()
   mathAlice.children() = List(examAlice, labAlice)
@@ -23,6 +23,19 @@ class Submission {
 
   val manualGrade: VarSynt[Option[Double]] = Var(None)
 
+  val childGrade: DependentSignal[Option[Double]] = Signal {
+    val grades = children().map { _.grade() }.flatten
+    if (grades.length > 0)
+      Some(grades.sum / grades.length)
+    else
+      None
+  }
+  
+  val childPass: DependentSignal[Boolean] = Signal {
+    val passes = children().map { _.pass() }
+    Functions.conjuction(passes)
+  }
+  
   val grade: DependentSignal[Option[Double]] = Signal {
     manualGrade() match {
       case Some(g) => Some(g)
@@ -33,22 +46,12 @@ class Submission {
           None
     }
   }
+  
   val pass: DependentSignal[Boolean] = Signal {
     grade() match {
       case None    => false
       case Some(g) => g >= 5.5
     }
-  }
-  val childGrade: DependentSignal[Option[Double]] = Signal {
-    val grades = children().map { _.grade() }.flatten
-    if (grades.length > 0)
-      Some(grades.sum / grades.length)
-    else
-      None
-  }
-  val childPass: DependentSignal[Boolean] = Signal {
-    val passes = children().map { _.pass() }
-    Functions.conjuction(passes)
   }
 }
 
@@ -58,3 +61,15 @@ object Functions {
     input.foldRight(true)((a, b) => a && b)
   }
 }
+
+/*
+The order of the attributes matters.
+grade, pass, childGrade, childPass causes a null pointer exception add runtime, on instantiating mathAlice.
+
+REScala does support definitions such as the one you mentioned.
+It is however limited by the rules for `val`s in Scala.
+In this case the problem is, that `grade` has a forward reference to `childPass` as well as `childGrade`.
+At the time when `grade` is initialized, both forward references are still uninitialized, leading to the NPE.
+You can add the `-Xcheckinit` to scalac to get runtime checks for these kinds of errors (which produces a better exception than just a NPE).
+The solution in this case is to move both the definition of `childPass` and `childGrade` before the definition of `grade`.
+*/
