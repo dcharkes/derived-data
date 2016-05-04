c02fe4dda980773e1fd3f0ac2d1c8bb4510b0ad9
Daco Harkes <dc.harkes@gmail.com>
2016-05-04 18:20:22 +0200

    non-recursive version of WebLab grade: i3QL: calculate grade and pass on the lowest level

--- Weblab-Grades-NonRecursive/i3QL/src/main/scala/WeblabGradesI3QL.scala
+++ Weblab-Grades-NonRecursive/i3QL/src/main/scala/WeblabGradesI3QL.scala
@@ -15,35 +15,75 @@ import scala.virtualization.lms.common.{Base, BaseExp}
 object WeblabGradesI3QL {
   type Id = Int
   type Submission = (Id, Option[Id], Option[String], Option[Double])
+  type SubmissionFull = (Id, Option[Id], Option[String], Option[Double], Option[Double], Boolean)
+  type SubmissionGrade = (Id, Option[Double])
+  type SubmissionPass = (Id, Boolean)
 
   val submission1 = SetTable.empty[Submission]
   val submission2 = SetTable.empty[Submission]
   val submission3 = SetTable.empty[Submission]
 
+  val submission3grade: Relation[SubmissionGrade] = (
+    SELECT(
+      (s: Rep[Submission]) => (
+        s._1,
+        s._4
+        ))
+      FROM (submission3)
+      WHERE ((s: Rep[Submission]) => true)
+    )
+  val submission3gradeMaterialized = submission3grade.asMaterialized
+
+  val submission3pass: Relation[SubmissionPass] = (
+    SELECT(
+      (s: Rep[SubmissionGrade]) => (
+        s._1,
+        if (s._2.isDefined) s._2.get >= 5.5 else false
+        ))
+      FROM (submission3grade)
+      WHERE (_ => true)
+    )
+  val submission3passMaterialized = submission3pass.asMaterialized
+
+  // should be : Relation[SubmissionFull], but its not
+  val submission3fullNested: Relation[(Submission, SubmissionGrade, SubmissionPass)] = (
+    SELECT(*)
+      FROM(submission3, submission3grade, submission3pass)
+      WHERE ((s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) =>
+      (s3._1 == s3grade._1 AND
+        s3._1 == s3pass._1))
+    )
+  val submission3fullNestedMaterialized = submission3fullNested.asMaterialized
+
+  // what is wrong with this relation? why does it not constitute a SubmissionFull?
+  //  // should be : Relation[SubmissionFull], but its not
+  //  val submission3full = (
+  //    SELECT(
+  //      (s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) => (
+  //        s3._1,
+  //        s3._2,
+  //        s3._3,
+  //        s3._4,
+  //        s3grade._2,
+  //        s3pass._2
+  //        ))
+  //      FROM(submission3, submission3grade, submission3pass)
+  //      WHERE ((s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) =>
+  //      (s3._1 == s3grade._1 AND
+  //        s3._1 == s3pass._1))
+  //    )
+  //  val submission3fullMaterialized = submission3full.asMaterialized
+
+  // should be : Relation[(Id, Option[Double])]
+  // SUM of ints, as AVG and doubles are not available. Note that toInt also needs a fix in the i3QL lib
+  // should use submission3full, once that works
   val submission2childGrade: Relation[(Id, Int)] = (
-    SELECT((s: Rep[Option[Int]]) => s.get, SUM((s: Rep[Submission]) => s._4.get.toInt)) //SUM of ints, as AVG and doubles are not available. Note that toInt also needs a fix in the i3QL lib
+    SELECT((s: Rep[Option[Int]]) => s.get, SUM((s: Rep[Submission]) => s._4.get.toInt))
       FROM (submission3)
       WHERE ((s: Rep[Submission]) => s._4.isDefined)
       GROUP BY((s: Rep[Submission]) => s._2)
     )
   val submission2childGradeMaterialized = submission2childGrade.asMaterialized
-
-  def gradeToPass(g: Option[Double]): Boolean = {
-    // how to lift this to Reps?
-    g match {
-      case None => false
-      case Some(g) => g >= 5.5
-    }
-  }
-
-  //  val submission3pass: Relation[(Id, Boolean)] = (
-  val submission3pass: Relation[(Id, Option[Double])] = (
-    //      SELECT((s: Rep[Submission]) => (s._1, gradeToPass(s._4)))
-    SELECT((s: Rep[Submission]) => (s._1, s._4))
-      FROM (submission3)
-      WHERE ((s: Rep[Submission]) => true)
-    )
-  val submission3passMaterialized = submission3pass.asMaterialized
 
   def main(args: Array[String]): Unit = {
     import Predef.println
@@ -69,13 +109,26 @@ object WeblabGradesI3QL {
     submission3 += lab1Bob
     submission3 += lab2Bob
 
+
     println()
-    println("submission2 (id, childGrade):")
-    submission2childGradeMaterialized.foreach(println(_))
+    println("submission3 (id, grade):")
+    submission3gradeMaterialized.foreach(println(_))
 
     println()
     println("submission3 (id, pass):")
     submission3passMaterialized.foreach(println(_))
+
+    println()
+    println("submission3 full:")
+    submission3fullNestedMaterialized.foreach(println(_))
+
+    //    println()
+    //    println("submission3 full:")
+    //    submission3fullMaterialized.foreach(println(_))
+
+    println()
+    println("submission2 (id, childGrade):")
+    submission2childGradeMaterialized.foreach(println(_))
 
   }
 }
