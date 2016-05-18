8f15ed0922d3d7c9076d99e98330031db06a7b8e
Daco Harkes <dc.harkes@gmail.com>
2016-05-18 11:57:21 +0200

    submission tree, non-recursive definition in i3QL

--- Weblab-SubmissionTree/i3QL/src/main/scala/WeblabSubmissionTree.scala
+++ Weblab-SubmissionTree/i3QL/src/main/scala/WeblabSubmissionTree.scala
@@ -9,126 +9,75 @@ import scala.reflect.SourceContext
 import scala.Some
 import scala.virtualization.lms.common.{Base, BaseExp}
 
-// make sure we don't use LMS Some's
-
-
 object WeblabSubmissionTree {
   type Id = Int
-  type Submission = (Id, Option[Id], Option[String], Option[Double])
-  type SubmissionFull = (Id, Option[Id], Option[String], Option[Double], Option[Double], Boolean)
-  type SubmissionGrade = (Id, Option[Double])
-  type SubmissionPass = (Id, Boolean)
+  type Assignment = (Id, String, Option[Id])
+  type Student = (Id, String)
+  // Submission = (Id, name, assignmentId, studentId)
+  type Submission = (Id, String, Id, Id)
 
-  val submission1 = SetTable.empty[Submission]
-  val submission2 = SetTable.empty[Submission]
-  val submission3 = SetTable.empty[Submission]
+  val assignment = SetTable.empty[Assignment]
+  val student = SetTable.empty[Student]
+  val submission = SetTable.empty[Submission]
 
-  val submission3grade: Relation[SubmissionGrade] = (
+  val submissionParent: Relation[(Id, Id)] = (
     SELECT(
-      (s: Rep[Submission]) => (
-        s._1,
-        s._4
+      (a1: Rep[Assignment], a2: Rep[Assignment], s1: Rep[Submission], s2: Rep[Submission]) => (
+        s1._1,
+        s2._1
         ))
-      FROM (submission3)
-      WHERE ((s: Rep[Submission]) => true)
+      FROM(assignment, assignment, submission, submission)
+      WHERE ((a1: Rep[Assignment], a2: Rep[Assignment], s1: Rep[Submission], s2: Rep[Submission]) =>
+      (a2._3.isDefined AND
+        a1._1 == a2._3.get AND
+        s1._3 == a1._1 AND
+        s2._3 == a2._1 AND
+        s1._4 == s2._4)
+      )
     )
-  val submission3gradeMaterialized = submission3grade.asMaterialized
-
-  val submission3pass: Relation[SubmissionPass] = (
-    SELECT(
-      (s: Rep[SubmissionGrade]) => (
-        s._1,
-        if (s._2.isDefined) s._2.get >= 5.5 else false
-        ))
-      FROM (submission3grade)
-      WHERE (_ => true)
-    )
-  val submission3passMaterialized = submission3pass.asMaterialized
-
-  // should be : Relation[SubmissionFull], but its not
-  val submission3fullNested: Relation[(Submission, SubmissionGrade, SubmissionPass)] = (
-    SELECT(*)
-      FROM(submission3, submission3grade, submission3pass)
-      WHERE ((s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) =>
-      (s3._1 == s3grade._1 AND
-        s3._1 == s3pass._1))
-    )
-  val submission3fullNestedMaterialized = submission3fullNested.asMaterialized
-
-  // what is wrong with this relation? why does it not constitute a SubmissionFull?
-  //  // should be : Relation[SubmissionFull], but its not
-  //  val submission3full = (
-  //    SELECT(
-  //      (s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) => (
-  //        s3._1,
-  //        s3._2,
-  //        s3._3,
-  //        s3._4,
-  //        s3grade._2,
-  //        s3pass._2
-  //        ))
-  //      FROM(submission3, submission3grade, submission3pass)
-  //      WHERE ((s3: Rep[Submission], s3grade: Rep[SubmissionGrade], s3pass: Rep[SubmissionPass]) =>
-  //      (s3._1 == s3grade._1 AND
-  //        s3._1 == s3pass._1))
-  //    )
-  //  val submission3fullMaterialized = submission3full.asMaterialized
-
-  // should be : Relation[(Id, Option[Double])]
-  // SUM of ints, as AVG and doubles are not available. Note that toInt also needs a fix in the i3QL lib
-  // should use submission3full, once that works
-  val submission2childGrade: Relation[(Id, Int)] = (
-    SELECT((s: Rep[Option[Int]]) => s.get, SUM((s: Rep[Submission]) => s._4.get.toInt))
-      FROM (submission3)
-      WHERE ((s: Rep[Submission]) => s._4.isDefined)
-      GROUP BY((s: Rep[Submission]) => s._2)
-    )
-  val submission2childGradeMaterialized = submission2childGrade.asMaterialized
+  val submissionParentMaterialized = submissionParent.asMaterialized
 
   def main(args: Array[String]): Unit = {
     import Predef.println
 
-    val mathAlice = new Submission(1, None, None, None)
-    val examAlice = new Submission(2, Some(mathAlice._1), Some("Good"), Some(8.0))
-    val labAlice = new Submission(3, Some(mathAlice._1), None, None)
-    val lab1Alice = new Submission(4, Some(labAlice._1), Some("Perfect"), Some(10.0))
-    val lab2Alice = new Submission(5, Some(labAlice._1), Some("Sufficient"), Some(9.0))
-    submission1 += mathAlice
-    submission2 += examAlice
-    submission2 += labAlice
-    submission3 += lab1Alice
-    submission3 += lab2Alice
-    val mathBob = new Submission(6, None, None, None)
-    val examBob = new Submission(7, Some(mathBob._1), Some("Very Good"), Some(9.0))
-    val labBob = new Submission(8, Some(mathBob._1), None, None)
-    val lab1Bob = new Submission(9, Some(labBob._1), Some("Insufficient"), Some(3.0))
-    val lab2Bob = new Submission(10, Some(labBob._1), Some("Perfect"), Some(10.0))
-    submission1 += mathBob
-    submission2 += examBob
-    submission2 += labBob
-    submission3 += lab1Bob
-    submission3 += lab2Bob
-
-
-    println()
-    println("submission3 (id, grade):")
-    submission3gradeMaterialized.foreach(println(_))
-
-    println()
-    println("submission3 (id, pass):")
-    submission3passMaterialized.foreach(println(_))
-
-    println()
-    println("submission3 full:")
-    submission3fullNestedMaterialized.foreach(println(_))
-
-    //    println()
-    //    println("submission3 full:")
-    //    submission3fullMaterialized.foreach(println(_))
+    val alice = new Student(1, "Alice")
+    val bob = new Student(2, "Bob")
+    student += alice
+    student += bob
+    val math = new Assignment(1, "math", None)
+    val exam = new Assignment(2, "exam", Some(1))
+    val lab = new Assignment(3, "lab", Some(1))
+    val lab1 = new Assignment(4, "lab1", Some(3))
+    val lab2 = new Assignment(5, "lab2", Some(3))
+    assignment += math
+    assignment += exam
+    assignment += lab
+    assignment += lab1
+    assignment += lab2
+    val mathAlice = new Submission(1, "mathAlice", 1, 1)
+    val examAlice = new Submission(2, "examAlice", 2, 1)
+    val labAlice = new Submission(3, "labAlice", 3, 1)
+    val lab1Alice = new Submission(4, "lab1Alice", 4, 1)
+    val lab2Alice = new Submission(5, "lab2Alice", 5, 1)
+    submission += mathAlice
+    submission += examAlice
+    submission += labAlice
+    submission += lab1Alice
+    submission += lab2Alice
+    val mathBob = new Submission(6, "mathBob", 1, 2)
+    val examBob = new Submission(7, "examBob", 2, 2)
+    val labBob = new Submission(8, "labBob", 3, 2)
+    val lab1Bob = new Submission(9, "lab1Bob", 4, 2)
+    val lab2Bob = new Submission(10, "lab2Bob", 5, 2)
+    submission += mathBob
+    submission += examBob
+    submission += labBob
+    submission += lab1Bob
+    submission += lab2Bob
 
     println()
-    println("submission2 (id, childGrade):")
-    submission2childGradeMaterialized.foreach(println(_))
+    println("submission parent (id, parent):")
+    submissionParentMaterialized.foreach(println(_))
 
   }
 }
@@ -140,7 +89,7 @@ To get the i3QL dependency working, one needs to locally build a certain version
 
       $ git clone https://github.com/seba--/virtualization-lms-core.git
 
-  Go to the root directory and install the project using SBT. You need to be on the branch 'i3QL-compatible'
+  Go to the root directory and install the project using SBT. You need to be on the branch "i3QL-compatible"
 
       $ cd virtualization-lms-core
       $ git checkout develop
@@ -150,11 +99,11 @@ To get the i3QL dependency working, one needs to locally build a certain version
 
       $ git clone https://github.com/seba--/i3QL.git
 
-  Go to the root directory and install the project using SBT. Currently you need to be on the branch 'scala-2.10.2'.
+  Go to the root directory and install the project using SBT. Currently you need to be on the branch "scala-2.10.2".
 
       $ cd i3Ql
       $ git checkout master
       $ sbt publish-local
 
-To run the code `sbt run` or in IntelliJ a sbt task `run` (running as application in IntelliJ doesn't work).
+To run the code `sbt run` or in IntelliJ a sbt task `run` (running as application in IntelliJ doesn"t work).
  */
