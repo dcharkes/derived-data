7103b21d2fddbb3bfab7121a0d9a47ecd63379ab
Daco Harkes <dc.harkes@gmail.com>
2016-05-24 13:35:40 +0200

    Rust data structure

--- Weblab-Grades-NonRecursive/Adapton.rust/WeblabGradesNonRecursive/src/main.rs
+++ Weblab-Grades-NonRecursive/Adapton.rust/WeblabGradesNonRecursive/src/main.rs
@@ -1,11 +1,32 @@
 fn main() {
-    let mut lab2Alice = Submission{ children:vec![], answer:"bla".to_string(), manual_grade:9.0 };
-    lab2Alice.manual_grade = 8.0;
+    let mut math_alice = Submission1 { children: vec![], answer: None, manual_grade: None };
+    let mut exam_alice = Submission2 { children: vec![], answer: Some("Good".to_string()), manual_grade: Some(8.0) };
+    let mut lab_alice = Submission2 { children: vec![], answer: None, manual_grade: None };
+    let mut lab1_alice = Submission3 { answer: Some("Perfect".to_string()), manual_grade: Some(10.0) };
+    let mut lab2_alice = Submission3 { answer: Some("Sufficient".to_string()), manual_grade: Some(6.0) };
+    math_alice.children = vec![exam_alice, lab_alice];
+    lab_alice.children = vec![lab1_alice, lab2_alice];
     println!("Hello, world!");
+    //    println!("{:?}", grade(lab1_alice));
 }
 
-struct Submission {
-    children: Vec<Submission>,
-    answer: String,
-    manual_grade: f32
+struct Submission1 {
+    children: Vec<Submission2>,
+    answer: Option<String>,
+    manual_grade: Option<f32>
+}
+
+struct Submission2 {
+    children: Vec<Submission3>,
+    answer: Option<String>,
+    manual_grade: Option<f32>
+}
+
+struct Submission3 {
+    answer: Option<String>,
+    manual_grade: Option<f32>
+}
+
+fn grade(s: &Submission3) -> Option<f32> {
+    s.manual_grade
 }
