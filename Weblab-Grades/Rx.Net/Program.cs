f103486b35edc321f81ccc9762bc2cbd7692f273
Daco Harkes <dc.harkes@gmail.com>
2016-06-08 19:01:56 +0200

    Rx.Net Weblab grades implementation (cleanup)

--- Weblab-Grades/Rx.Net/Program.cs
+++ Weblab-Grades/Rx.Net/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -16,34 +15,42 @@ namespace WeblabGrades
             var labAlice = new Submission();
             var lab1Alice = new Submission();
             var lab2Alice = new Submission();
-            mathAlice.children2.OnNext(new List<Submission> { examAlice, labAlice });
-            labAlice.children2.OnNext(new List<Submission> { lab1Alice, lab2Alice });
-            examAlice.answer2.OnNext("Good");
-            examAlice.manualGrade2.OnNext(8.0f);
-            examAlice.answer2.OnNext("Perfect");
-            lab1Alice.manualGrade2.OnNext(10.0f);
-            examAlice.answer2.OnNext("Sufficient");
-            lab2Alice.manualGrade2.OnNext(6.0f);
+            mathAlice.children.OnNext(new[] { examAlice, labAlice });
+            labAlice.children.OnNext(new[] { lab1Alice, lab2Alice });
+            examAlice.answer.OnNext("Good");
+            examAlice.manualGrade.OnNext(8.0f);
+            examAlice.answer.OnNext("Perfect");
+            lab1Alice.manualGrade.OnNext(10.0f);
+            examAlice.answer.OnNext("Sufficient");
+            lab2Alice.manualGrade.OnNext(6.0f);
             var mathBob = new Submission();
             var examBob = new Submission();
             var labBob = new Submission();
             var lab1Bob = new Submission();
             var lab2Bob = new Submission();
-            mathBob.children2.OnNext(new List<Submission> { examBob, labBob });
-            labBob.children2.OnNext(new List<Submission> { lab1Bob, lab2Bob });
-            examBob.answer2.OnNext("Very Good");
-            examBob.manualGrade2.OnNext(9.0f);
-            examBob.answer2.OnNext("Insufficient");
-            lab1Bob.manualGrade2.OnNext(3.0f);
-            examBob.answer2.OnNext("Perfect");
-            lab2Bob.manualGrade2.OnNext(10.0f);
+            mathBob.children.OnNext(new[] { examBob, labBob });
+            labBob.children.OnNext(new[] { lab1Bob, lab2Bob });
+            examBob.answer.OnNext("Very Good");
+            examBob.manualGrade.OnNext(9.0f);
+            examBob.answer.OnNext("Insufficient");
+            lab1Bob.manualGrade.OnNext(3.0f);
+            examBob.answer.OnNext("Perfect");
+            lab2Bob.manualGrade.OnNext(10.0f);
 
             Console.WriteLine("Alice");
-            Console.WriteLine(mathAlice.grade2.Latest().First());
-            Console.WriteLine(mathAlice.pass2.Latest().First());
+            Console.WriteLine(mathAlice.grade.Latest().First());
+            Console.WriteLine(mathAlice.pass.Latest().First());
             Console.WriteLine("Bob");
-            Console.WriteLine(mathBob.grade2.Latest().First() + "Null");
-            Console.WriteLine(mathBob.pass2.Latest().First());
+            Console.WriteLine(mathBob.grade.Latest().First() + "Null");
+            Console.WriteLine(mathBob.pass.Latest().First());
+            Console.WriteLine("");
+
+            lab1Bob.answer.OnNext("Sufficient");
+            lab1Bob.manualGrade.OnNext(6.0f);
+
+            Console.WriteLine("Bob");
+            Console.WriteLine(mathBob.grade.Latest().First());
+            Console.WriteLine(mathBob.pass.Latest().First());
             Console.WriteLine("");
 
             Console.WriteLine("Press any key to continue");
@@ -54,88 +61,117 @@ namespace WeblabGrades
     public class Submission
     {
 
-        public BehaviorSubject<List<Submission>> children2; //note List is mutable, should replace list, not mutate it
+        public BehaviorSubject<IEnumerable<Submission>> children;
 
-        public BehaviorSubject<string> answer2;
+        public BehaviorSubject<string> answer;
 
-        public BehaviorSubject<float?> manualGrade2;
+        public BehaviorSubject<float?> manualGrade;
 
-        public IObservable<float?> childGrade2;
+        public IObservable<float?> childGrade;
 
-        public IObservable<bool> childPass2;
+        public IObservable<bool> childPass;
 
-        public IObservable<float?> grade2;
+        public IObservable<float?> grade;
 
-        public IObservable<bool> pass2;
+        public IObservable<bool> pass;
 
         public Submission()
         {
-            children2 = new BehaviorSubject<List<Submission>>(new List<Submission>());
+            children = new BehaviorSubject<IEnumerable<Submission>>(Enumerable.Empty<Submission>());
 
-            answer2 = new BehaviorSubject<string>("");
+            answer = new BehaviorSubject<string>("");
 
-            manualGrade2 = new BehaviorSubject<float?>(null);
+            manualGrade = new BehaviorSubject<float?>(null);
 
-            IObservable<List<IObservable<float?>>> childGrades1 = children2.Select(xs => xs.Select(x => x.grade2).ToList());
-            IObservable<IObservable<List<float?>>> childGrades2 = childGrades1.Select(x => Sequence(x));
-            IObservable<List<float?>> childGrades3 = childGrades2.Merge();
-            IObservable<List<float>> childGrades4 = childGrades3.Select(x => x.Where(y => y.HasValue).Select(y => y.Value).ToList());
-            childGrade2 = childGrades4.Select(x => x.Any() ? x.Average() : (float?)null);
+            IObservable<IEnumerable<float>> grades = children.Select(xs => xs.Select(x => x.grade).CombineLatestSafe()).Merge().Select(x => x.Flatten());
+            childGrade = grades.Select(x => x.AverageSafe());
 
-            IObservable<List<bool>> childPasses = children2.Select(xs => Sequence<bool>(xs.Select(x => x.pass2).ToList())).Merge();
-            childPass2 = childPasses.Select(x => FoldL((a, b) => a && b, true, x));
+            IObservable<IEnumerable<bool>> passes = children.Select(xs => xs.Select(x => x.pass).CombineLatestSafe()).Merge();
+            childPass = passes.Select(xs => xs.Conjunction());
 
-            grade2 = manualGrade2.CombineLatest(childGrade2, (mg2, cg2) => Tuple.Create(mg2, cg2)).CombineLatest(childPass2, (tuple, cp2) =>
-             {
-                 var mg2 = tuple.Item1;
-                 var cg2 = tuple.Item2;
-                 if (mg2.HasValue)
-                     return mg2;
-                 else
-                 {
-                     if (cp2)
-                         return cg2;
-                     else
-                         return null;
-                 }
-             });
-
-            pass2 = grade2.CombineLatest(childPass2, (g2, cp2) =>
+            grade = manualGrade.CombineLatest(childGrade, childPass, (manualGrade1, childGrade1, childPass1) =>
             {
-                var gradePass = g2.HasValue ? g2 >= 5.5f : false;
-                return gradePass && cp2;
+                if (manualGrade1.HasValue)
+                    return manualGrade1;
+                else
+                {
+                    if (childPass1)
+                        return childGrade1;
+                    else
+                        return null;
+                }
+            });
+
+            pass = grade.CombineLatest(childPass, (grade1, childPass1) =>
+            {
+                var gradePass = grade1.HasValue ? grade1 >= 5.5f : false;
+                return gradePass && childPass1;
             });
 
         }
 
-        public static IObservable<List<T>> Sequence<T>(List<IObservable<T>> elems)
+    }
+
+    public static class MyExtensions
+    {
+        /**
+         * Conjunction of a list of bools
+         */
+        public static bool Conjunction(this IEnumerable<bool> elems)
         {
-            return FoldL((acc, o) =>
-            {
-                return acc.CombineLatest(o, (acc1, o1) =>
-                {
-                    var z = new List<T>();
-                    z.AddRange(acc1);
-                    z.Add(o1);
-                    return z;
-                });
-            }, Observable.Return(new List<T>()), elems);
+            return elems.FoldL(true, (a, b) => a && b);
         }
 
+        /**
+         * Average of floats returing an optional float (null if imput list is empty)
+         */
+        public static float? AverageSafe(this IEnumerable<float> elems)
+        {
+            return elems.Any() ? elems.Average() : (float?)null;
+        }
+
+        /**
+         * Flatten for list of optional floats
+         */
+        public static IEnumerable<float> Flatten(this IEnumerable<float?> elems)
+        {
+            return elems.Where(x => x.HasValue).Select(x => x.Value);
+        }
+
+        /**
+         * Merges the specified observable sequences into one observable sequence by emitting
+         *     a list with the latest source elements whenever any of the observable sequences
+         *     produces an element.
+         *     
+         * (Original implementation doesn't work with empty lists)
+         */
+        public static IObservable<IEnumerable<T>> CombineLatestSafe<T>(this IEnumerable<IObservable<T>> elems)
+        {
+            return elems.FoldL(
+                Observable.Return(Enumerable.Empty<T>()),
+                (acc, o) => acc.CombineLatest(o, (acc1, o1) => acc1.Append(o1)));
+        }
+
+        /**
+         * Append a single element
+         */
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> elems, T elem)
+        {
+            return elems.Concat(new[] { elem });
+        }
+
+        /**
+         * FoldL extension method
+         */
+        public static A FoldL<A, B>(this IEnumerable<B> b, A a, Func<A, B, A> f)
+        {
+            return FoldL(f, a, b);
+        }
+
+        /**
+         * Tail recursive FoldL (haskell style parameters)
+         */
         public static A FoldL<A, B>(Func<A, B, A> f, A a, IEnumerable<B> b)
-        {
-            if (!b.Any())
-                return a;
-            else
-            {
-                var first = b.First();
-                var aprime = f(a, first);
-                var brest = b.Skip(1);
-                return FoldL(f, aprime, brest);
-            }
-        }
-
-        public static A FoldL2<A, B>(Func<A, B, A> f, A a, IEnumerable<B> b)
         {
             var acc = a;
             var bs = b;
