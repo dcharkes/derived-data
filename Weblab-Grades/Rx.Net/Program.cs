79ec76927e42e6910ac3fc81d7b5cd5ef7759676
Daco Harkes <dc.harkes@gmail.com>
2016-06-08 16:03:40 +0200

    Rx.Net explore IObservable & BehaviorSubject API

--- Weblab-Grades/Rx.Net/Program.cs
+++ Weblab-Grades/Rx.Net/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
 namespace WeblabGrades
 {
@@ -20,7 +22,6 @@ namespace WeblabGrades
             labAlice.children.Add(lab2Alice);
             examAlice.answer = "Good";
             examAlice.manualGrade = 8.0f;
-            // examAlice.manualGrade2.
             lab1Alice.answer = "Perfect";
             lab1Alice.manualGrade = 10.0f;
             lab2Alice.answer = "Sufficient";
@@ -47,6 +48,19 @@ namespace WeblabGrades
             Console.WriteLine("Bob");
             Console.WriteLine(mathBob.grade() + "Null");
             Console.WriteLine(mathBob.pass());
+            Console.WriteLine("");
+            Console.WriteLine("Alice");
+            Console.WriteLine(mathAlice.manualGrade2.Value+" ");
+            Console.WriteLine(mathAlice.grade2.Latest().First()+" ");
+            Console.WriteLine(mathAlice.pass2.Latest().First()+ " ");
+            mathAlice.manualGrade2.OnNext(6.7f);
+            Console.WriteLine(mathAlice.manualGrade2.Value + " ");
+            Console.WriteLine(mathAlice.grade2.Latest().First() + " ");
+            Console.WriteLine(mathAlice.pass2.Latest().First() + " ");
+            mathAlice.manualGrade2.OnNext(8.9f);
+            Console.WriteLine(mathAlice.manualGrade2.Value + " ");
+            Console.WriteLine(mathAlice.grade2.Latest().First() + " ");
+            Console.WriteLine(mathAlice.pass2.Latest().First() + " ");
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
@@ -81,10 +95,12 @@ namespace WeblabGrades
             if (manualGrade.HasValue)
                 return manualGrade;
             else
+            {
                 if (childPass())
-                return childGrade();
-            else
-                return null;
+                    return childGrade();
+                else
+                    return null;
+            }
         }
 
         public bool pass()
@@ -93,11 +109,13 @@ namespace WeblabGrades
             return gradePass && childPass();
         }
 
-        public IObservable<float?> manualGrade2;
+        public BehaviorSubject<List<Submission>> children2; //note List is mutable, but replace with new lists...
+
+        public BehaviorSubject<float?> manualGrade2;
 
         public IObservable<float?> childGrade2;
 
-        public IObservable<bool> childPass2;
+        public BehaviorSubject<bool> childPass2;
 
         public IObservable<float?> grade2;
 
@@ -105,12 +123,53 @@ namespace WeblabGrades
 
         public Submission()
         {
+            children2 = new BehaviorSubject<List<Submission>>(new List<Submission>());
+
+            manualGrade2 = new BehaviorSubject<float?>(null);
+
+            childPass2 = new BehaviorSubject<bool>(true); //default is no children
+
+            IObservable<List<IObservable<float?>>> childGrades1 = children2.Select(xs => xs.Select(x => x.grade2).ToList());
+            IObservable<IObservable<List<float?>>> childGrades2 = childGrades1.Select(x => Sequence(x));
+            IObservable<List<float?>> childGrades3 = childGrades2.Merge();
+            childGrade2 = new BehaviorSubject<float?>(null);
+
+            grade2 = manualGrade2.CombineLatest(childGrade2, (mg2, cg2) => Tuple.Create(mg2, cg2)).CombineLatest(childPass2, (tuple, cp2) =>
+             {
+                 var mg2 = tuple.Item1;
+                 var cg2 = tuple.Item2;
+                 if (mg2.HasValue)
+                     return mg2;
+                 else
+                 {
+                     if (cp2)
+                         return cg2;
+                     else
+                         return null;
+                 }
+             });
+
             pass2 = grade2.CombineLatest(childPass2, (g2, cp2) =>
             {
                 var gradePass = g2.HasValue ? g2 >= 5.5f : false;
                 return gradePass && cp2;
             });
+
+        }
+
+        public static IObservable<List<T>> Sequence<T>(List<IObservable<T>> elems)
+        {
+            List<IObservable<List<T>>> a = elems.Select(x => x.Select(y => new List<T> { y })).ToList();
+            IObservable<List<T>> b = a.Aggregate((x, y) => x.CombineLatest(y, (x1, y1) =>
+            {
+                var z = new List<T>();
+                z.AddRange(x1);
+                z.AddRange(y1);
+                return z;
+            }));
+            return b;
         }
 
     }
+
 }
