9b51a6187ba6fa575f3096e52152bb395adbc74d
Daco Harkes <dc.harkes@gmail.com>
2016-07-05 19:11:04 +0200

    weblab grades: plain Java - access modifiers

--- Weblab-Grades/Java/src/Submission.java
+++ Weblab-Grades/Java/src/Submission.java
@@ -2,36 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 
-public class Submission {
+class Submission {
 
     private List<Submission> children = new ArrayList<Submission>();
 
-    public void addChild(Submission c){
+    void addChild(Submission c) {
         children.add(c);
     }
 
     private String answer = "";
 
-    public void setAnswer(String a){
+    void setAnswer(String a) {
         answer = a;
     }
 
     private Double manualGrade = null;
 
-    public void setManualGrade(Double m){
+    void setManualGrade(Double m) {
         manualGrade = m;
     }
 
-    public Double getChildGrade() {
+    Double getChildGrade() {
         OptionalDouble childGrade = children.stream().map(Submission::getGrade).filter(g -> g != null).mapToDouble(g -> g).average();
         return childGrade.isPresent() ? childGrade.getAsDouble() : null;
     }
 
-    public boolean getChildPass() {
+    boolean getChildPass() {
         return children.stream().map(Submission::getPass).reduce(true, (Boolean a, Boolean b) -> a && b);
     }
 
-    public Double getGrade() {
+    Double getGrade() {
         if (manualGrade != null)
             return manualGrade;
         if (getChildPass())
@@ -39,10 +39,8 @@ public class Submission {
         return null;
     }
 
-    public boolean getPass() {
-        if (getGrade() != null)
-            return getGrade() >= 5.5;
-        return false;
+    boolean getPass() {
+        return getGrade() != null && getGrade() >= 5.5;
     }
 
 }
