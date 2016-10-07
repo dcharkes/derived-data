0a62b18f2e8e1e4236a7ebe32bf5252b125235d8
Frank McSherry <fmcsherry@me.com>
2016-10-07 14:09:05 +0200

    don't commit target

--- Weblab-Grades/differential-dataflow/target/debug/differential_dataflow.d
+++
@@ -1,5 +0,0 @@
-/Users/mcsherry/Projects/derived-data/Weblab-Grades/differential-dataflow/target/debug/differential_dataflow: src/main.rs
-
-/Users/mcsherry/Projects/derived-data/Weblab-Grades/differential-dataflow/target/debug/differential_dataflow.d: src/main.rs
-
-src/main.rs:
